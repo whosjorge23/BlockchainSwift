28b03e6a52a8ab44a21f3ae94949dfd0e510131b
Giorgio Giannotta <jorge_upa@msn.com>
2021-03-22 16:13:27 +0100

    Initial Commit

---
+++ Blockchain/BlockchainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainApp.swift
+//  Blockchain
+//
+//  Created by Jorge Giannotta on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blockchain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blockchain
+//
+//  Created by Jorge Giannotta on 22/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
